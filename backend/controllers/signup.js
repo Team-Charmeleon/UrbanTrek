4196ab50959553af5befd8f5b0bcae2ff59ca8fe
lumengli7 <131196081+lumengli7@users.noreply.github.com>
2024-03-06 10:05:04 -0600

    changed response object for signup

--- backend/controllers/signup.js
+++ backend/controllers/signup.js
@@ -1,5 +1,6 @@
 import { query } from '../models/models.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, password } = req.body;
@@ -25,8 +26,26 @@ export const signup = async (req, res, next) => {
     const values = [username, hashedPass];
 
     const result = await query.query(queryString, values);
-    console.log('successful!', result);
-    next();
+
+    const fetchQueryString = `
+    SELECT * FROM "public"."users" WHERE users.username = $1`;
+
+    query.query(fetchQueryString, [username], (err, result) => {
+      if (err) {
+        return res.status(400).json({ err: `Could not find user ${err}` });
+      }
+      const user = result.rows[0];
+      const userId = user.user_id;
+
+      const payload = { userId };
+
+      const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '15m',
+      });
+
+      res.locals.signup = { userId, accessToken };
+      next();
+    });
   } catch (error) {
     console.log(error);
     return res.status(400).json({ error: `Could not create user ${error}` });
